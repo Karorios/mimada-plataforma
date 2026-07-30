817473f398c708709504ea5cda0add254f6da866
KaroRios <hkrios10@ucatolica.edu.co>
2026-07-30 10:35:33 -0500

    Creacion Home y seccion catalogo con datos de prueba-falta completar catalogo con los productos y enlazarlos con la tabla de inventario

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'catalogo'
-
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('lista/', views.lista_productos, name='lista'),
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from catalogo.models import Producto
+from catalogo.models import Producto, Categoria
 
 
 def inicio(request):
-    productos_destacados = Producto.objects.filter(
-        destacado=True,
-        disponible=True
-    )
+    productos_destacados = Producto.objects.all()
+    categorias = Categoria.objects.all()
     return render(request, 'catalogo/inicio.html', {
         'productos_destacados': productos_destacados,
+        'categorias': categorias,
+    })
+
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+    categorias = Categoria.objects.all()
+    return render(request, 'catalogo/lista.html', {
+        'productos': productos,
+        'categorias': categorias,
     })
