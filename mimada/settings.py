d0c047c881e8c9227399003226878949366d7bab
KaroRios <hkrios10@ucatolica.edu.co>
2026-08-29 09:43:57 -0500

    Agregar WhiteNoise y STATIC_ROOT para servir estaticos en produccion

--- mimada/settings.py
+++ mimada/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     'pedidos',
     'finanzas',
     'inventario',
-    'panelprincipaladmin',
     'pedidosadmin',
     'asistente_ia',
 ]
--- panelprincipaladmin/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-imagen = models.ImageField(
-    upload_to='catalogo/productos/',
-    blank=True,
-    null=True
-)
